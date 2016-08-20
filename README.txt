83ced78328af572a2bed523cb48f443a095d3dfa
Theodore Baschak <theodore@ciscodude.net>
2016-08-20 00:19:25 -0500

    small updates on master branch

--- README.txt
+++ README.txt
@@ -1,29 +1,29 @@
 ===================================
-EIONET OTRS SKIN
+HEXTET OTRS SKIN
 ===================================
 
 Installation
 ============
-In order to install the EIONET OTRS Skin, go to the Custom directory in your OTRS
+In order to install the HEXTET OTRS Skin, go to the Custom directory in your OTRS
 deployment directory (e.g.: /opt/otrs/Custom)::
 
 $ cd /opt/otrs/Custom
-$ git clone https://github.com/eea/eionet.otrs.theme
+$ git clone https://github.com/HextetSystems/hextet.otrs.theme
 
 Edit install.sh and make sure OTRS_USER, OTRS_GROUP and OTRS_ROOT variables are set correctly for your environment, then::
 
 $ sudo ./install.sh
 
 Log into OTRS system with the admin user, go to SysConfig **Core::Web** and set: 
-    **Loader::Agent::DefaultSelectedSkin** to eionet
+    **Loader::Agent::DefaultSelectedSkin** to hextet
 then go to **Frontend::Customer** and set:
-    **Loader::Customer::SelectedSkin** to eionet
+    **Loader::Customer::SelectedSkin** to hextet
 
-In order to be able to use our own favicon.ico, we need to change **Frontend::ImagePath** to <OTRS_CONFIG_Frontend::WebPath>skins/Agent/eionet/img/
+In order to be able to use our own favicon.ico, we need to change **Frontend::ImagePath** to <OTRS_CONFIG_Frontend::WebPath>skins/Agent/hextet/img/
 
 OTRS Theme:
 
-To activate the theme in otrs, we need to go to SysConfig Core::Web and add our EIONET theme to Frontend::Themes and make it active.
-To make our theme the default theme, we need to go to SysConfig Core and change the *DefaultTheme* setting to **EIONET** . This will apply our theme to all users that have not previously set the Theme preference in their user preferences.
+To activate the theme in otrs, we need to go to SysConfig Core::Web and add our HEXTET theme to Frontend::Themes and make it active.
+To make our theme the default theme, we need to go to SysConfig Core and change the *DefaultTheme* setting to **HEXTET** . This will apply our theme to all users that have not previously set the Theme preference in their user preferences.
 
 If we want the theme default theme setting to override user's preferences, then we need to set our default theme on a host level *DefaultTheme::HostBased*.
--- install.sh
+++ install.sh
@@ -5,29 +5,30 @@ set -e
 #The following variables are user configurable
 SKIN_PATH=`/bin/pwd`
 OTRS_USER="otrs"
-OTRS_GROUP="apache"
+OTRS_GROUP="www-data"
 OTRS_ROOT="/opt/otrs"
 
 #make theme directory
-/bin/mkdir -p $OTRS_ROOT/Kernel/Output/HTML/EIONET
+/bin/mkdir -p $OTRS_ROOT/Kernel/Output/HTML/HEXTET
 
 #Fix permissions and make the proper symlinks
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $SKIN_PATH
-/bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/EIONET
+/bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/HEXTET
 /usr/bin/find $SKIN_PATH/skins/ -type f -print0 | xargs -0 /bin/chmod 0660
 /usr/bin/find $SKIN_PATH/skins/ -type d -print0 | xargs -0 /bin/chmod 2770
 
-/bin/ln -s $SKIN_PATH/EionetSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
-/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Config/Files/EionetSkin.xml
-/bin/ln -s $SKIN_PATH/skins/Agent/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
-/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Agent/eionet
-/bin/ln -s $SKIN_PATH/skins/Customer/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
-/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Customer/eionet
+/bin/ln -s $SKIN_PATH/HextetSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Config/Files/HextetSkin.xml
+/bin/ln -s $SKIN_PATH/skins/Agent/hextet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Agent/hextet
+/bin/ln -s $SKIN_PATH/skins/Customer/hextet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Customer/hextet
 
-/bin/cp -r $SKIN_PATH/theme/EIONET $OTRS_ROOT/Kernel/Output/HTML
+/bin/cp -r $SKIN_PATH/theme/HEXTET $OTRS_ROOT/Kernel/Output/HTML
+/bin/cp -r $SKIN_PATH/theme/HEXTET $OTRS_ROOT/Kernel/Output/HTML/Templates
 
 echo ""
 echo "==========================================================="
-echo "All set. Now you can use the eionet skin and theme in otrs"
+echo "All set. Now you can use the hextet skin and theme in otrs"
 echo "==========================================================="
 echo ""
